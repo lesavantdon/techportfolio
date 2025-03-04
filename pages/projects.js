0f081f9cf6fbbbe2a0703fb6a7eff94dd1948256
Aneet Singh Sahota <aneetsingh@Aneets-MacBook-Pro.local>
2025-03-03 21:14:54 -0800

    final projects

--- pages/projects.js
+++ pages/projects.js
@@ -4,9 +4,9 @@ import React from 'react';
 const Projects = () => {
   return (
     <div>
-      <h1 style={{ textDecoration: "underline", textAlign: "center", marginBottom: "40px" }}>Project Page</h1>
+      <h1 style={{ textDecoration: "underline", textAlign: "center", marginBottom: "0px" }}>Project Page</h1>
       <div>
-      <h2>1. Personalized Fitness App Tracker  </h2>
+      <h2 style={{ textDecoration: "underline", textAlign: "center", marginBottom: "20px" }}>1. Personalized Fitness App Tracker  </h2>
       <ul> This is a fitness app and tracker to shuffle different exercises based on the day of the week. This allows me to streamline my workout and map out my workout week without the needed foresight. In addition, a journal log to track and record different workouts based on the genre of the week, to log all data and track progress inorder to refine my health regimine all with the touch of a finger.    </ul>
 
       <video width="750" controls>
@@ -15,7 +15,7 @@ const Projects = () => {
       </video>
     </div>
       <div>
-      <h2>2. Greenhouse Plant Tracker </h2>
+      <h2 style={{ textDecoration: "underline", textAlign: "center", marginBottom: "20px" }}>2. Greenhouse Plant Tracker </h2>
       <ul>This is my personalized greenhouse web app that will allow me to educate myself with different plants from the TREFLE API and access important plant care instructions. In addition, I will be able to create a greenhouse with my plants and track data on it that gives it the optimal environment to grow in without excessive intervention. These plants will eventually be tracked with sensor data and a calendar with appropriate feeding schedules, and alerts that will be sent to your phone.</ul>
 
       <video width="750" controls>
